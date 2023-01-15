f8f7becdab1eceb9d363044a1ca03f9a939b7954
Valentina Carpentiero <carpentierovalentina8@gmail.com>
2023-01-15 16:53:49 +0100

    Exercise - SQL-alter

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+
+public class Main {
+    static final String URL = "jdbc:mysql://localhost:3306/newdb";
+    static final String USER = "developer";
+    static final String PASSWORD = "userpasw";
+    static final String ALTER_TABLE = "ALTER TABLE students ADD country VARCHAR(30) NOT NULL";
+
+
+    public static void main(String[] args) {
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = conn.createStatement();
+             PreparedStatement preparedStatement = conn.prepareStatement("UPDATE students SET country = ? WHERE student_id = ?")) {
+
+               statement.executeUpdate(ALTER_TABLE);
+
+               insertCountry(preparedStatement, "Italy",1);
+               insertCountry(preparedStatement,"Italy",2);
+               insertCountry(preparedStatement,"Germany",3);
+               insertCountry(preparedStatement,"Germany",4);
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void insertCountry(PreparedStatement preparedStatement, String country, int studentId) throws SQLException {
+        preparedStatement.setString(1, country);
+        preparedStatement.setInt(2, studentId);
+        preparedStatement.executeUpdate();
+    }
+}
